7f8df5575f7986bd44a7aa00d1be16b560771753
Michael Kogan <mike@sliday.com>
2020-05-13 03:28:31 -0500

    guests controller refactored

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,61 +1,25 @@
 class GuestsController < ApplicationController
-  before_action :set_guest, only: [:show, :edit, :update, :destroy]
-
   def index
-    @guests = Guest.all
-    @guest = Guest.new
-  end
-
-  def show
-  end
-
-  def new
     @guest = Guest.new
   end
 
   def edit
+    @guest = Guest.find(params[:id])
   end
 
   def create
     @guest = Guest.new(guest_params)
 
-    respond_to do |format|
-      if @guest.save
-        format.html { redirect_to root_path, notice: 'Готово!' }
-        format.json { render :index, status: :created, location: @guest }
-      else
-        format.html { render :new }
-        format.json { render json: @guest.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def update
-    respond_to do |format|
-      if @guest.update(guest_params)
-        format.html { redirect_to @guest, notice: 'Готово!' }
-        format.json { render :show, status: :ok, location: @guest }
-      else
-        format.html { render :edit }
-        format.json { render json: @guest.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @guest.destroy
-    respond_to do |format|
-      format.html { redirect_to guests_url, notice: 'Гость удален' }
-      format.json { head :no_content }
+    if @guest.save
+      redirect_to root_path, notice: 'Будем вас ждать!'
+    else
+      render :index
     end
   end
 
   private
-    def set_guest
-      @guest = Guest.find(params[:id])
-    end
 
-    def guest_params
-      params.require(:guest).permit(:name, :message, :number_of_guests)
-    end
+  def guest_params
+    params.require(:guest).permit(:name, :message, :number_of_guests)
+  end
 end
